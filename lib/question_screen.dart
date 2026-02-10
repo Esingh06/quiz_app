dea094360dfafe7539fcdad2c37bb7057796bb1d
Eikamvir Singh <eikamvir10@gmail.com>
2026-02-10 12:03:24 -0500

    Startscreen update

---
+++ lib/question_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget
+{
+  const QuestionScreen({super.key});
+
+  State<QuestionScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+class _QuestionScreenState extends State<QuestionScreen>{
+  @override
+  Widget build(BuildContext context){
+    throw UnimplementedError();
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+
+  
+  State<Quiz> createState(){
+ return _QuizState();
+  }
+   
+}
+  
+
+class _QuizState extends State<Quiz>{
+  Widget? activeScreen;
+  void swithcScreen(){
+    setState(() {
+      activeScreen = QuestionScreen();
+    });
+  }
+
+  void initState(){
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context){
+   return activeScreen;
+  }
+}

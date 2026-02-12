ffc2fa577c2ae9658876bdaab257fac7d20e03ed
Eikamvir Singh <eikamvir10@gmail.com>
2026-02-12 11:27:02 -0500

    Example Questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,30 @@
+import 'package:quiz_app/models/quiz_app_sp26.dart';
+const questions = [
+  
+QuizQuestion('How did Edward Elric lose his arm in FMA?'
+,
+[]
+  'Puttinh his brother sould in suit of armor',
+  'In a Duel'
+  'In a freak Dabbing Accident
+  'He never had any arms',
+  
+]),
+QuizQuestion
+('What is the name of the main protagonist in FMA?'
+,
+[
+  'Edward Elric',
+  'Alphonse Elric',
+  'Roy Mustang',
+  'Winry Rockbell'
+]),
+QuizQuestion
+('What is the name of the main protagonist in FMA?'
+,
+[
+  'Edward Elric',
+  'Alphonse Elric',
+  'Roy Mustang',
+  'Winry Rockbell'
+]),
